98bb49b8147f0b6abb9b910bee3f69ec86feb22e
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2023-05-08 16:27:16 +0530

    Update __init__.py

--- clone/__init__.py
+++ clone/__init__.py
@@ -16,12 +16,6 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
 
-
-API_ID = int(environ["API_ID"])
-API_HASH = environ["API_HASH"]
-BOT_TOKEN = environ["BOT_TOKEN"]
-
-
 #os.system("apt install git curl python3-pip ffmpeg -y")
 
 class Mbot(Client):
